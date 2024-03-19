01c30d3fbd88f571bd637f6171ba99ea5da4e7f6
gohgrace02 <gohgrace02@gmail.com>
2024-03-19 13:53:12 +0800

    Updated back_project.py to receive AMQP messages

--- microservices/Complex/back_project.py
+++ microservices/Complex/back_project.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify, redirect, url_for, render_template
 from flask_cors import CORS
+import amqp_connection
 import json
+import pika
 
 import os, sys
 
@@ -8,6 +10,8 @@ import requests
 
 app = Flask(__name__)
 CORS(app)
+
+a_queue_name = 'Back_Project' # queue to be subscribed by Activity_Log microservice
 
 project_URL = "http://localhost:5000/project"
 tracker_URL = "http://localhost:5001/project"
@@ -23,6 +27,7 @@ def list_projects():
     projects = response['data']['projects']
     return render_template('list_projects.html', projects=projects)
 
+
 @app.route("/back_project/<string:projectID>")
 def project_details(projectID):
     response = requests.get(project_URL + '/' + projectID).json()
@@ -32,7 +37,35 @@ def project_details(projectID):
 # @app.route("/back_project/<string:projectID>")
 
 
+def receiveFulfilmentLog(channel):
+    try:
+        # set up a consumer and start to wait for coming messages
+        channel.basic_consume(queue=a_queue_name, on_message_callback=callback, auto_ack=True)
+        print('back_project: Consuming from queue:', a_queue_name)
+        channel.start_consuming()  # an implicit loop waiting to receive messages;
+             #it doesn't exit by default. Use Ctrl+C in the command window to terminate it.
+    
+    except pika.exceptions.AMQPError as e:
+        print(f"back_project: Failed to connect: {e}") # might encounter error if the exchange or the queue is not created
+
+    except KeyboardInterrupt:
+        print("back_project: Program interrupted by user.") 
+
+
+def callback(channel, method, properties, body): # required signature for the callback; no return
+    print("\nback_project: Received a fulfilment log by " + __file__)
+    processFulfilmentLog(json.loads(body))
+    print()
+
+
+def processFulfilmentLog(fulfilment):
+    print("back_project: Recording a fulfilment log:")
+    print(fulfilment)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5004, debug=True)
-
+    connection = amqp_connection.create_connection() #get the connection to the broker
+    print("back_project: Connection established successfully")
+    channel = connection.channel()
+    receiveFulfilmentLog(channel)  # Start consuming messages from the queue
