625e6a38b11abce435aabed708e3e8d9e0448f7d
Joycelyn <joycelynwee.2022@scis.smu.edu.sg>
2024-04-01 16:15:43 +0800

    update tracker.py

--- microservices/Tracker/tracker.py
+++ microservices/Tracker/tracker.py
@@ -137,8 +137,8 @@ def create_tracker(project_id):
     user_id = data.get('user_id')
     payment_intent_id = data.get('payment_intent_id')
     # check goal_reached status of project
-    url = "http://localhost:5000/project/" + str(project_id)
-    # url = "http://project:5000/project/" + str(project_id)
+    # url = "http://localhost:5000/project/" + str(project_id)
+    url = "http://project:5000/project/" + str(project_id)
     goal_reached = requests.get(url).json()['data']['goal_reached']
 
     # 'captured' value is set to False if goal not reached
@@ -172,8 +172,8 @@ def create_tracker(project_id):
         ), 500
     
     # Send a GET request to Project microservice to get the funding_goal
-    # project_URL = "http://project:5000/project"
-    project_URL = "http://localhost:5000/project"
+    project_URL = "http://project:5000/project"
+    # project_URL = "http://localhost:5000/project"
     response = requests.get(project_URL + '/' + str(project_id)).json()
     data = response['data']
     funding_goal = response['data']['funding_goal']
