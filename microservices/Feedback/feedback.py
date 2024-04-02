2a6d9ed713137ed25a27dadcc324b6bbc5d41ecc
gohgrace02 <gohgrace02@gmail.com>
2024-04-02 21:27:48 +0800

    update config

--- microservices/Feedback/feedback.py
+++ microservices/Feedback/feedback.py
@@ -4,14 +4,15 @@ from os import environ
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL')
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:8889/feedback'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_recycle': 299}
 
 db = SQLAlchemy(app)
 
+CORS(app)
 
 class Feedback(db.Model):
     __tablename__ = 'feedback'
